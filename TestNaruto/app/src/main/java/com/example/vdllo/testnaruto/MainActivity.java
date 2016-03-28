90669ee3139f881f983cd189587cf7c5cf2ec927
small-seven <353193036@qq.com>
2016-03-28 19:13:18 +0800

    点击功能完成

--- TestNaruto/app/src/main/java/com/example/vdllo/testnaruto/MainActivity.java
+++ TestNaruto/app/src/main/java/com/example/vdllo/testnaruto/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.vdllo.testnaruto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "小胡小胡,下雨不愁", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     //创建好了
@@ -20,4 +31,6 @@ public class MainActivity extends AppCompatActivity {
     //这我都知道
 
     //happy
+
+
 }

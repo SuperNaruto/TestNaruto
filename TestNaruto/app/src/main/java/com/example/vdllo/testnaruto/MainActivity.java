b8efd5943cfa92e38420dfb0d59c92743f85ffb1
small-seven <353193036@qq.com>
2016-03-28 19:27:36 +0800

    跳转页面完成

--- TestNaruto/app/src/main/java/com/example/vdllo/testnaruto/MainActivity.java
+++ TestNaruto/app/src/main/java/com/example/vdllo/testnaruto/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.vdllo.testnaruto;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +9,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn;
+    private Button btn,my_btn;
     private ImageView imageView;
 
     @Override
@@ -17,6 +18,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn = (Button) findViewById(R.id.btn);
+        my_btn = (Button) findViewById(R.id.my_btn);
+        my_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,NextActivity.class);
+                startActivity(intent);
+            }
+        });
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
---
+++ TestNaruto/app/src/main/java/com/example/vdllo/testnaruto/NextActivity.java
@@ -0,0 +1,15 @@
+package com.example.vdllo.testnaruto;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dllo on 16/3/28.
+ */
+public class NextActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+    }
+}
